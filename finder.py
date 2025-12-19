2578997f2c355c0846b845c9ae93abe054fb944a
timothyoswald <nxie88@gmail.com>
2025-12-19 01:26:11 -0500

    very bad and sometimes infinite looping lvl 9 finder

--- finder.py
+++ finder.py
@@ -28,13 +28,46 @@ def makeGraph(units, traits):
             G.add_edge(unitA, unitB, weight = len(sharedTraits), shared = list(sharedTraits))
     return G
 
+# for now, score should be based on unit cost
+# and traits contributed to the board
+# there is an issue where score changes
+# as new units are added
+def computeScore(G, allTraits, boardTraits, newUnit):
+    score = G.nodes[newUnit]["cost"]
+    for trait in G.nodes[newUnit]["traits"]:
+        if trait in boardTraits:
+            boardTraits[trait] += 1
+            num = boardTraits[trait]
+            if str(num) in allTraits[trait]:
+                score += num
+        else:
+            boardTraits[trait] = 1
+    return score
+
 # given a carry find a level 9 comp around it
 def findCompositionWithCarry(G, carry, traits):
     currentBoard = {carry}
+    totalScore = G.nodes[carry]["cost"]
+    traitsSoFar = {trait: 1 for trait in G.nodes[carry]["traits"]}
 
     while len(currentBoard) < 9:
-        potentialUnits = set()
-        pass
+        bestOption = None
+        bestScore = 0
+        # look at all current neighbors and choose best one
+        # should definitely make this more efficient to not 
+        # relook through all neighbors
+        for unit in currentBoard:
+            for nbor in G.neighbors(unit):
+                score = computeScore(G, traits, traitsSoFar, nbor)
+                if score > bestScore:
+                    bestOption = nbor
+                    bestScore = score
+        currentBoard.add(bestOption)
+        totalScore += bestScore
+
+    return currentBoard, totalScore
+
 
 units, traits = loadData()
-makeGraph(units, traits)
+G = makeGraph(units, traits)
+print(findCompositionWithCarry(G, "Kindred", traits))
